ee81fecd8a88882a1e88fdcb12fd81fa097c3c49
Artem Shevchenko <shevartsoft@gmail.com>
2017-02-24 11:27:59 +0200

    add bottom navigation bar add vector icons and wrappers

---
+++ app/src/main/java/com/shevart/fitnessnotes/ui/dashboard/MainDashboardFragment.java
@@ -0,0 +1,17 @@
+package com.shevart.fitnessnotes.ui.dashboard;
+
+
+import com.shevart.fitnessnotes.R;
+import com.shevart.fitnessnotes.ui.base.BaseFragment;
+
+public class MainDashboardFragment extends BaseFragment {
+
+
+    public MainDashboardFragment() {
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.fragment_main_dasboard;
+    }
+}
